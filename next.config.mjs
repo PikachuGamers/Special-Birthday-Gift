1317e6eac8718bc29216ad30a535aef99b5c5752
PikachuGamers <pikachugamers119@gmail.com>
2026-04-15 15:26:15 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/Special-Birthday-Gift', // Aapki repository ka exact naam
   images: {
     unoptimized: true,
   },
