b562d57f9fa3c921272e3e2aebaf3e50d98d52d4
PikachuGamers <pikachugamers119@gmail.com>
2026-04-15 17:55:24 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,9 +8,17 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        className={`antialiased`}
-      >
+      <head>
+        {/* --- PREMIUM FONTS IMPORT START --- */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link 
+          href="https://fonts.googleapis.com/css2?family=Playpen+Sans:wght@700;800&family=Grand+Hotel&display=swap" 
+          rel="stylesheet" 
+        />
+        {/* --- PREMIUM FONTS IMPORT END --- */}
+      </head>
+      <body className="antialiased">
         {children}
       </body>
     </html>
